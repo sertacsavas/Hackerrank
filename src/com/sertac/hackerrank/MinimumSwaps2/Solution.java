355c85f0c1fe2acf394364dfdc757de7c3e88a39
Sertaç Savaş <sertacsavas@hotmail.com>
2020-10-21 19:41:11 +0300

    solution

--- src/com/sertac/hackerrank/MinimumSwaps2/Solution.java
+++ src/com/sertac/hackerrank/MinimumSwaps2/Solution.java
@@ -7,9 +7,25 @@ import java.util.Scanner;
 
 public class Solution {
 
-	// Complete the minimumSwaps function below.
 	static int minimumSwaps(int[] arr) {
-		return 0;
+		int swapCount = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != i + 1) {
+				swapCount++;
+				int index = 0;
+				for (int j = i + 1; j < arr.length; j++) {
+					if (arr[j] == i + 1) {
+						index = j;
+						break;
+					}
+				}
+				int temp = arr[i];
+				arr[i] = arr[index];
+				arr[index] = temp;
+			}
+		}
+
+		return swapCount;
 	}
 
 	private static final Scanner scanner = new Scanner(System.in);
